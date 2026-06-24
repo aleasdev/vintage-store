a7b8a9aee626c9f91e48ad1901c647531c102e58
aleasdev <alejandroeduardocb@gmail.com>
2026-06-24 09:21:30 -0400

    Change some texts

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -38,7 +38,7 @@ export default function SignInPage() {
         className="border p-2 rounded"
         required
       />
-      <input type="text" name="test" id="test" placeholder="test locotron" />
+      <input type="text" name="test" id="test" placeholder="test" />
       <button type="submit" className="bg-black text-white p-2 rounded">
         Sign In
       </button>
